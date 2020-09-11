26106c9617a9474f7160cad44b53c11c28f835b4
faroeq-linux-new <alves_1044@hotmail.com>
2020-09-11 18:40:33 +0200

    just made CategoryController

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/categories', 'CategoryController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
