e4b217fdbe59d609b57ecc072da4ff7e2b11cc62
faroeq-linux-new <alves_1044@hotmail.com>
2020-09-12 11:06:20 +0200

    categories are visible

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -63,17 +63,13 @@
     <!-- Begin page content -->
     <main role="main" class="flex-shrink-0">
         <div class="container">
-            <h1 class="mt-5">title</h1>
-            <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and
-                CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the
-                <code>main &gt; .container</code>.</p>
-            <p>Back to <a href="/docs/4.4/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+            @yield('content')
         </div>
     </main>
 
     <footer class="footer mt-auto py-3">
         <div class="container">
-            <span class="text-muted">Place sticky footer content here.</span>
+            <span class="text-muted">Laravel workshop &copy;</span>
         </div>
     </footer>
     <script src="{{asset('js/app.js')}}"></script>
