999bb9033d83cef7339e717e84bcc7e7a4975100
faroeq-linux-new <alves_1044@hotmail.com>
2020-09-25 09:36:09 +0200

    seeder added

---
+++ database/seeds/RoleAndPermissionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()['cache']->forget('spatie.permission.cache');
+
+        Permission::create(['name' => 'create category']);
+        Permission::create(['name' => 'edit category']);
+        Permission::create(['name' => 'delete category']);
+
+        $role = Role::create(['name' => 'customer']);
+
+        $role = Role::create(['name' => 'sales']);
+        $role->giverPermissionTo('create category', 'edit category');
+
+        $role = Role::create(['name' => 'sales']);
+        $role->givePermissionTo(Permission::all());
+    }
+}
